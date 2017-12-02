c78c89490a40318cacd3ce4f99bbeccb47db0af1
pedrorodrigues <pedrorodrigues@pedrorodrigues-HP-Pavilion-Gaming-Notebook>
2017-12-02 01:28:51 +0000

    Timers almost done

--- Teste_SELE/Master.c
+++ Teste_SELE/Master.c
@@ -14,250 +14,231 @@
  + diodo RX + ciclo de teste dos slaves */
 
 #include "Master_functions.h"
-/*
- ISR (TIMER1_OVF_vect) {
 
- TCNT1 = 0xC2F7; 	// renicia o timer com 49911
-
- if(1 == flag_Vermelho_1)
- {
- cont_Vermelho_1++;
- }
- if(1 == flag_Vermelho_2)
- {
- cont_Vermelho_2++;
- }
- if(1 == flag_Verde_1)
- {
- cont_Verde_1++;
- }
- if(1 == flag_Verde_2)
- {
- cont_Verde_2++;
- }
-
- if(cont_tempo >= 180)
- {
- //escrever na eeprom
- }
- }
- */
 int main(void) {
+
 	uint8_t aux = 0; /*variavel dos ciclos for */
 	uint8_t state = 0;
 	uint8_t cont_SM = 0; /* contador incremental para percorrer os slaves */
-	uint8_t check = 0;
 
 	uint8_t lotacao_MAX = 6;
-	uint8_t lotacao_atual = 0;
+	char lotacao_atual = 0;
 	uint8_t lotacao_atual_percentagem = 0;
 	uint8_t n_slaves = 2; /* número de slaves */
 
-	//uint8_t id_slave_sistema[2] = {0x01,0x02};
-	uint8_t id_slave_alive[2] = { 0x01, 0x02 }; //{0x00,0x00}; ALTERAR ISTOOOOOOOOOO DEPOIS DE CHEKAR QUE FUNCIONA
-	uint8_t valor_contador_slave[2] = { 0, 0 };
+	uint8_t id_slave_sistema[2] = { 0x01, 0x02 };
+	uint8_t id_slave_alive[2] = { 0x99, 0x99 }; //{0x00,0x00}; ALTERAR ISTOOOOOOOOOO DEPOIS DE CHEKAR QUE FUNCIONA
+	char valor_contador_slave[2] = { 0, 0 };
+	char byte = 0;
 
 	/* Inicialização */
 
 	init_io();
 	init_RS485();
-	/*init_timer_T1();
-	 init_interrupt();*/
+	init_timer_T1();
+	init_interrupt();
 
 	/* Modo configuração */
-/*
+	/*
 
-	if(!(PINB & (1 << Conf_buttom)))
-	{
-		LED_Vermelho_ON;
-		LED_Amarelo_ON;
-		while(1);
+	 if(!(PINB & (1 << Conf_buttom)))
+	 {
+	 LED_Vermelho_ON;
+	 LED_Amarelo_ON;
+	 while(1);
 
-	}
-*/
-	/* Rotina de checkar os slaves
-	 for( aux = 0 ; aux < n_slaves ; aux++)
-	 {
-	 if(0 == check_slave(id_slave_sistema[aux]))
-	 {
-	 id_slave_alive[aux] = id_slave_sistema[aux];
-	 }
-	 else
-	 {
-	 id_slave_alive[aux] = 0x00;
-	 }
 	 }
 	 */
+
+	/* Rotina de checkar os slaves */
+
+	for (aux = 0; aux < n_slaves; aux++) {
+
+		if (0 == check_slave(id_slave_sistema[aux])) {
+
+			id_slave_alive[aux] = id_slave_sistema[aux];
+
+		} else {
+
+			id_slave_alive[aux] = 0x00;
+
+		}
+		//if((id_slave_alive[0] == 0x01 )&& (id_slave_alive[1] == 0x02)) LED_Verde_ON;
+		_delay_us(30);
+	}
+
 	/* Máquina de estados */
 	while (1) {
 
-		switch (state){
+		switch (state) {
 
-			case STATE_ADDR_SEND:
+		case STATE_ADDR_SEND:
 
-				MAX485_Sending;
-
-				if (0x00 != id_slave_alive[cont_SM]) {
-					check = send_Address(id_slave_alive[cont_SM]);
-					state = STATE_CONT_RECEIVE;
-				} else {
-					if (cont_SM < (n_slaves - 1)) {
-						cont_SM++;
-					} else {
-						cont_SM = 0;
-					}
-				}
-				break;
-
-			case STATE_CONT_RECEIVE:
-
-				if(0 == check){
-					MAX485_Receiving;
-					valor_contador_slave[cont_SM] = RS485_receiveByte();
-					state = STATE_CALC_SEND;
-				}
-				break;
-
-			case STATE_CALC_SEND:
-
-				lotacao_atual = 0;
-
-				for (aux = 0; aux <= (n_slaves - 1); aux++) {
-					lotacao_atual = lotacao_atual + valor_contador_slave[aux];
-				}
-
-				lotacao_atual_percentagem = lotacao_atual * 100 / lotacao_MAX;
-
-				_delay_us(30);
-				MAX485_Sending;
-				if (lotacao_atual >= lotacao_MAX) {
-					//flag_Verde = 0;
-					//flag_Vermelho = 1;
-					check = send_Lotacao(1); /* Lotação superior à lotação MAX */
-					/*semaforo vermelho */
-					LED_Vermelho_ON;
-					LED_Amarelo_OFF;
-					LED_Verde_OFF;
-
-				} else if ((100 > lotacao_atual_percentagem) && (lotacao_atual_percentagem >= 81)) {
-					check = send_Lotacao(0);
-					//flag_Verde = 1;
-					//flag_Vermelho = 0;
-					/* semaforo amarelo */
-					LED_Vermelho_OFF;
-					LED_Amarelo_ON;
-					LED_Verde_OFF;
-				} else {
-					check = send_Lotacao(0);/* verificar o check */
-					//flag_Verde = 1;
-					//flag_Vermelho = 0;
-					/* semaforo verde */
-					LED_Vermelho_OFF;
-					LED_Amarelo_OFF;
-					LED_Verde_ON;
-				}
-
-				if (cont_SM < (n_slaves - 1)) {
-					cont_SM++;
-				} else {
-					cont_SM = 0;
-				}
-				state = STATE_ADDR_SEND;
-				_delay_us(200);
-				break;
-
-			default:
-
-				state = STATE_ADDR_SEND;
-				break;
-		}
-	}
-
-
-/*
-	while (1) {
-		if (0 == state) {
-			MAX485_Sending;
+			MAX485_Sending; /* Colocar pino de controlo a 1 -> MAX485 em sending mode */
 
 			if (0x00 != id_slave_alive[cont_SM]) {
-				check = send_Address(id_slave_alive[cont_SM]);
-				state = 1;
+
+				send_Address(id_slave_alive[cont_SM]);
+
+				state = STATE_CONT_RECEIVE;
+
 			} else {
-				if (cont_SM < n_slaves) {
+
+				if (cont_SM < (n_slaves - 1)) {
+
 					cont_SM++;
+
 				} else {
+
 					cont_SM = 0;
 				}
 			}
+			break;
 
-		} else if ((0 == check) && (1 == state)) {
+		case STATE_CONT_RECEIVE:
 
-			MAX485_Receiving;
-			valor_contador_slave[id_slave_alive[cont_SM]] = RS485_receiveByte();
-			state = 2;
-		} else if (2 == state) {
+			MAX485_Receiving
+			/* Colocar pino de controlo a 0 -> MAX485 em receiving mode */
+			;
+
+			byte = RS485_receiveByte();
+
+			if (watchdog_timeout == byte) {
+
+				id_slave_alive[cont_SM] = 0x00;
+
+				state = STATE_ADDR_SEND;
+				break;
+
+			} else {
+
+				valor_contador_slave[cont_SM] = byte;
+
+				state = STATE_CALC_SEND;
+				break;
+
+			}
+		case STATE_CALC_SEND:
 
 			lotacao_atual = 0;
 
-			for (aux = 0; aux < n_slaves; aux++) {
+			for (aux = 0; aux <= (n_slaves - 1); aux++) {
+
 				lotacao_atual = lotacao_atual + valor_contador_slave[aux];
 			}
 
 			lotacao_atual_percentagem = lotacao_atual * 100 / lotacao_MAX;
 
+			_delay_us(30);
+
 			MAX485_Sending;
 
-			 _delay_us(5);
-			if (lotacao_atual >= lotacao_MAX) {
-				//flag_Verde = 0;
-				//flag_Vermelho = 1;
-				check = send_Lotacao(1);  Lotação superior à lotação MAX
-				semaforo vermelho
-				LED_Vermelho_ON
-				;
-				LED_Amarelo_OFF
-				;
-				LED_Verde_OFF
-				;
+			if (lotacao_atual >= lotacao_MAX) { /* Lotação superior à lotação MAX */
 
-			} else if (100 > lotacao_atual_percentagem
-					&& lotacao_atual_percentagem >= 81) {
-				check = send_Lotacao(0);
-				//flag_Verde = 1;
-				//flag_Vermelho = 0;
-				 semaforo amarelo
-				LED_Vermelho_OFF
-				;
-				LED_Amarelo_ON
-				;
-				LED_Verde_OFF
-				;
+				if ( watchdog_timeout == send_Lotacao(1)) {
+
+					id_slave_alive[cont_SM] = 0x00;
+
+					state = STATE_ADDR_SEND;
+					break;
+
+				} else {
+
+					LED_Vermelho_ON
+					;
+					LED_Amarelo_OFF
+					;
+					LED_Verde_OFF
+					;
+
+					state = STATE_ADDR_SEND;
+
+					if (cont_SM < (n_slaves - 1)) {
+
+						cont_SM++;
+
+					} else {
+
+						cont_SM = 0;
+					}
+
+					_delay_us(200);
+					break;
+				}
+
+			} else if ((100 > lotacao_atual_percentagem)
+					&& (lotacao_atual_percentagem >= 81)) {
+
+				if ( watchdog_timeout == send_Lotacao(0)) {
+
+					id_slave_alive[cont_SM] = 0x00;
+					state = STATE_ADDR_SEND;
+					break;
+
+				} else {
+
+					LED_Vermelho_OFF
+					;
+					LED_Amarelo_ON
+					;
+					LED_Verde_OFF
+					;
+
+					state = STATE_ADDR_SEND;
+
+					if (cont_SM < (n_slaves - 1)) {
+
+						cont_SM++;
+
+					} else {
+
+						cont_SM = 0;
+					}
+
+					_delay_us(200);
+					break;
+				}
+
 			} else {
-				check = send_Lotacao(0); verificar o check
-				//flag_Verde = 1;
-				//flag_Vermelho = 0;
-				 semaforo verde
-				LED_Vermelho_OFF
-				;
-				LED_Amarelo_OFF
-				;
-				LED_Verde_ON
-				;
+
+				if ( watchdog_timeout == send_Lotacao(0)) {
+
+					id_slave_alive[cont_SM] = 0x00;
+					LED_Vermelho_ON;
+					state = STATE_ADDR_SEND;
+					break;
+
+				} else {
+					LED_Vermelho_OFF
+					;
+					LED_Amarelo_OFF
+					;
+					LED_Verde_ON
+					;
+
+					state = STATE_ADDR_SEND;
+
+					if (cont_SM < (n_slaves - 1)) {
+
+						cont_SM++;
+
+					} else {
+
+						cont_SM = 0;
+					}
+
+					_delay_us(200);
+					break;
+				}
 			}
 
-			if (cont_SM < n_slaves) {
-				cont_SM++;
-			} else {
-				cont_SM = 0;
-			}
-			state = 0;
+
+		default:
+
+			state = STATE_ADDR_SEND;
+			break;
+
 		}
-
 	}
-*/
-
 }
-
-
-
 
--- Teste_SELE/Master_functions.c
+++ Teste_SELE/Master_functions.c
@@ -8,6 +8,35 @@
 
 #include "Master_functions.h"
 
+volatile uint8_t watchdog = 0;
+volatile uint8_t watchdog_flag = 0;
+
+ISR (TIMER1_OVF_vect) {
+
+	watchdog_flag++;
+
+	if (watchdog > 5) {
+
+		LED_Vermelho_ON;
+		watchdog_flag = 1;
+
+		return;
+
+	} else {
+
+		return;
+
+	}
+}
+
+void reset_watchdog (void)
+{
+	TCNT1 = 0xC2F7; 	// renicia o timer com 49911
+	watchdog = 0;
+
+	return;
+}
+
 void init_io(void) {
 
 	/* DDRD |= (1 << Led_controlo_MAX485); */
@@ -31,39 +60,54 @@ void init_RS485(void) {
 
 	/* Set frame format pagina 194: 9data, 1stop bit sem paridade*/
 	UCSR0C = (7 << UCSZ00)/*(1<<USBS0)seria para 2 stop bits */
-			| (0 << UPM00) /* no parity */
-			| (0 << USBS0) /* 1 stop bit */
-			| (0 << UMSEL00) | (0 << UMSEL01);/* comunicacao assincrona */
-	/* direction */
+	| (0 << UPM00) /* no parity */
+	| (0 << USBS0) /* 1 stop bit */
+	| (0 << UMSEL00) | (0 << UMSEL01);/* comunicacao assincrona */
+
 	DDRB |= (1 << controlo_MAX485);/* Definir pino como saída PB2 */
 }
 
 void RS485_sendByte(uint8_t temp) {
-	/* Wait for empty transmit buffer */
+
 	while ((UCSR0A & (1 << UDRE0)) == 0)
+		/* Wait for empty transmit buffer */
 		;
-	/* Put data into buffer, sends the data */
-	UDR0 = temp;
+
+	UDR0 = temp; /* Put data into buffer, sends the data */
+
 	UCSR0A |= 1 << TXC0;
+
 	while ((UCSR0A & (1 << TXC0)) == 0)
-		; /* espera até ter enviado o byte */
-}
-
-uint8_t RS485_receiveByte(void) {
-	/* Wait for data to be received */
-	while ((UCSR0A & (1 << RXC0)) == 0)
+		/* espera até ter enviado o byte */
 		;
-	/* Get and return received data from buffer */
-	return UDR0;
 }
 
-uint8_t send_Address(uint8_t n_slave) {
+char RS485_receiveByte(void) {
+
+	/* Wait for data to be received */
+
+	reset_watchdog();
+
+	while ((UCSR0A & (1 << RXC0)) == 0) {
+
+		if (1 == watchdog_flag) {
+
+			return watchdog_timeout;
+
+		}
+	}
+
+	return UDR0;/* Get and return received data from buffer */
+
+}
+
+void send_Address(uint8_t n_slave) {
 
 	UCSR0B = UCSR0B | (1 << TXB80); /* Trama de endereços */
 
 	RS485_sendByte(n_slave);
 
-	return 0;
+	return;
 }
 
 uint8_t send_Lotacao(uint8_t semaforo) {
@@ -71,58 +115,77 @@ uint8_t send_Lotacao(uint8_t semaforo) {
 	UCSR0B = UCSR0B & ~(1 << TXB80); /* Trama de dados */
 
 	if (1 == semaforo) {
-		RS485_sendByte(0xFF);/* Parque ocupado - semaforo vermelho */
-		MAX485_Receiving
-		;
-		if (0xFF == RS485_receiveByte()) {
-			return 0;
-		} else {
-			return -1;
-		}
-	} else if (0 == semaforo) {
-		RS485_sendByte(0xAA); /* Parque livre - semaforo verde */
-		MAX485_Receiving
-		;
-		if (0xAA == RS485_receiveByte()) {
-			return 0;
-		} else {
-			return -1;
-		}
-	} else {
-		return 1;
-	}
 
+		RS485_sendByte(0xFF);/* Parque ocupado - semaforo vermelho */
+
+		MAX485_Receiving
+		;
+
+		if (0xFF == RS485_receiveByte()) {
+
+			return 0;
+
+		} else {
+
+			return watchdog_timeout;
+
+		}
+
+	} else {
+
+		RS485_sendByte(0xAA); /* Parque livre - semaforo verde */
+
+		MAX485_Receiving
+		;
+
+		if (0xAA == RS485_receiveByte()) {
+
+			return 0;
+
+		} else {
+
+			return watchdog_timeout;
+
+		}
+
+	}
 }
 
 uint8_t check_slave(uint8_t n_slave) {
-	if (0 == send_Address(n_slave)) {
-		if (0 == RS485_receiveByte()) {
-			return 0;
-		} else {
-			return 1;
-		}
+
+	MAX485_Sending;
+	send_Address(n_slave);
+	MAX485_Receiving
+	;
+
+	if (n_slave == RS485_receiveByte()) {
+
+		return 0;
+
 	} else {
-		return 1;
+
+		return watchdog_timeout;
+
 	}
+
 }
-/*
- void init_timer_T1(void) {
 
- //este timer conta segundos, escala = 1s
+void init_timer_T1(void) {
 
- TCCR1B = 0;		//parar TC1
- TCCR1A = 0;		//modo normal
- TCNT1 = 0xC2F7;	//valor para iniciar o timer (65536 - (1 - 16MHz / 1024)) = 4991
+	//este timer conta segundos, escala = 1s
 
- TIMSK1 |= (1 << TOIE1);	//ativar a interupção por overflow
+	TCCR1B = 0;		//parar TC1
+	TCCR1A = 0;		//modo normal
+	TCNT1 = 0xC2F7;	//valor para iniciar o timer (65536 - (1 - 16MHz / 1024)) = 4991
 
- TCCR1B |= (1 << CS12) | (1 << CS10);		// timer 1 com predivisão de 1024
- }
+	TIMSK1 |= (1 << TOIE1);	//ativar a interupção por overflow
 
- void init_interrupt(void) {
- //definir interrupções
- sei();
+	TCCR1B |= (1 << CS12) | (1 << CS10);	// timer 1 com predivisão de 1024
+}
 
- }
- */
+void init_interrupt(void) {
+
+	sei();//definir interrupções
+
+}
 
